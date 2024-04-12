0220fe41cbde668cd00ac5895af0eaf2cd8684dd
Htet Aung <chindwin.98@icloud.com>
2024-04-12 06:34:26 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -60,7 +60,8 @@ class Dev(Configuration):
         'allauth.socialaccount',
         'allauth.socialaccount.providers.google',
         'rest_framework',
-        'rest_framework.authtoken'
+        'rest_framework.authtoken',
+        'drf_yasg'
     ]
 
     REST_FRAMEWORK = {
@@ -72,6 +73,13 @@ class Dev(Configuration):
       "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
       ],
+    }
+
+    SWAGGER_SETTINGS = {
+        "SECURITY_DEFINITIONS": {
+            "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+            "Basic": {"type": "basic"},
+        }
     }
 
     SITE_ID = 1
