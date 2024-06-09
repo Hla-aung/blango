2e8e1f54d18cc41a7e37fb82034d3e5f72c3c09a
Htet Aung <chindwin.98@icloud.com>
2024-06-09 16:11:35 +0000

    Finish JavaScript classes

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -63,3 +63,29 @@ const doubled = numbers.map(value => value * 2)
 console.log('Here are the doubled numbers')
 
 console.log(doubled)
+
+
+class Greeter {
+  constructor (name) {
+    this.name = name
+  }
+
+  getGreeting () {
+    if (this.name === undefined) {
+      return 'Hello, no name'
+    }
+
+    return 'Hello, ' + this.name
+  }
+
+  showGreeting (greetingMessage) {
+    console.log(greetingMessage)
+  }
+
+  greet () {
+    this.showGreeting(this.getGreeting())
+  }
+}
+
+const g = new Greeter('Patchy')  // Put your name here if you like
+g.greet()
